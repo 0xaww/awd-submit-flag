ce1bea318bc9f2d18325144016f510e1834ae63f
吴疆 <297913194@qq.com>
2018-09-13 11:52:19 +0800

    add new

--- automatic.py
+++ automatic.py
@@ -4,20 +4,23 @@ import target
 import exploit
 import submit
 import log
+import time
 
 def clean():
     os.system("rm *.pyc")
 
 
 def main():
-    startip , endip , port ,skip_ip , ip_list = target.main()
-    for ip in ip_list:
-        if ( ip in skip_ip):
-            continue
-        message = exploit.main(ip,port)
-        submit.main(ip,message,token=0)
-        log.main(ip,message)
-    clean()
+    while 1:
+        startip , endip , port ,skip_ip , ip_list = target.main()
+        for ip in ip_list:
+            if ( ip in skip_ip):
+                continue
+            message = exploit.main(ip,port)
+            submit.main(ip,message,token=0)
+            log.main(ip,message)
+        clean()
+        time.sleep(300)#set round time
 
 if __name__ == '__main__':
     main()
